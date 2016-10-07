e4fd6fdeb80b67f82bb076e2571cd4c619a91071
Dan Le <dhl1337@gmail.com>
2016-10-07 12:57:39 -0500

    add es6 to profile controller

--- app/components/profile/ProfileController.js
+++ app/components/profile/ProfileController.js
@@ -1,22 +1,15 @@
-/**
- * Created by danle on 1/10/16.
- */
-(function () {
+(() => {
     angular
         .module('favourApp')
         .controller('ProfileController', ['MainService','$stateParams',ProfileController]);
 
     function ProfileController (MainService, $stateParams) {
-        var vm = this;
-        var currentUserId = $stateParams.userId;
+        const vm = this;
+        const currentUserId = $stateParams.userId;
 
         vm.user = MainService.getUser(currentUserId);
 
-        console.log(vm.user);
-
-        $('.ui.rating')
-            .rating('disable')
-        ;
+        $('.ui.rating').rating('disable');
 
     }
 })();
